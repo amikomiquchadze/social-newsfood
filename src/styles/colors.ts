e1c77ea2d0ce5ee1a4ddf23d71ef52573334dcda
amiko <amikomiquchadze1@gmail.com>
2025-06-20 03:29:39 +0400

    extend colors

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,11 +1,13 @@
 export const Colors = {
-    white: "#ffff",
-    green: "#32B45F",
-    black: "#181d27",
-    gray: "#535862",
-    disabledwhite: "#F5F5F5",
-    darkgrey: "#A4A7AE",
-    lighgray: "#717680",
-    red: "#D92D20",
-    graytext: "#414651"
-}
+  white: "#ffff",
+  green: "#32B45F",
+  black: "#181d27",
+  gray: "#535862",
+  disabledwhite: "#F5F5F5",
+  darkgrey: "#A4A7AE",
+  lighgray: "#717680",
+  red: "#D92D20",
+  graytext: "#414651",
+  lightgreen: "#00813A",
+  darkgreen: "#1b5e20",
+};
