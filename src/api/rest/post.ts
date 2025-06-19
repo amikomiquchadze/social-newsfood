ac8fefd274c5c79505789ebd2b9fcc8f7d486d90
amiko <amikomiquchadze1@gmail.com>
2025-06-19 15:52:19 +0400

    fix: adjust apis

--- src/api/rest/post.ts
+++ src/api/rest/post.ts
@@ -1,80 +1,38 @@
 import client from "../client";
 import { CreatePostPayload } from "../models/payload/post";
 import { Post } from "../models/response/post";
-import { API_TOKEN } from "./comment";
 
 export const createPost = async (payload: CreatePostPayload): Promise<Post> => {
-  const response = await client.post<Post>("/Post/Create", payload, {
-    headers: {
-      "Content-Type": "application/json",
-      "X-Token": API_TOKEN,
-    },
-  });
+  const response = await client.post<Post>("/Post/Create", payload);
   return response.data;
 };
 
 export const fetchPosts = async (): Promise<Post[]> => {
-  const response = await client.post<Post[]>(
-    "/Post/GetAll",
-    {},
-    {
-      headers: {
-        "Content-Type": "application/json",
-        "X-Token": API_TOKEN,
-      },
-    }
-  );
+  const response = await client.post<Post[]>("/Post/GetAll", {});
   return response.data;
 };
 
 export const deletePost = async (postId: number): Promise<void> => {
-  await client.post(
-    "/Post/Delete",
-    {
-      PostID: postId,
-    },
-    {
-      headers: {
-        "Content-Type": "application/json",
-        "X-Token": API_TOKEN,
-      },
-    }
-  );
+  await client.post("/Post/Delete", {
+    PostID: postId,
+  });
 };
 export const reactToPost = async (
   postId: number,
   reactionType: string
 ): Promise<void> => {
-  await client.post(
-    "/Post/ReactionToggle",
-    {
-      PostID: postId,
-      ReactionType: reactionType,
-    },
-    {
-      headers: {
-        "Content-Type": "application/json",
-        "X-Token": API_TOKEN,
-      },
-    }
-  );
+  await client.post("/Post/ReactionToggle", {
+    PostID: postId,
+    ReactionType: reactionType,
+  });
 };
 
 export const updatePost = async (
   postId: number,
   content: string
 ): Promise<void> => {
-  await client.post(
-    "/Post/Update",
-    {
-      PostID: postId,
-      Content: content,
-    },
-    {
-      headers: {
-        "Content-Type": "application/json",
-        "X-Token": API_TOKEN,
-      },
-    }
-  );
+  await client.post("/Post/Update", {
+    PostID: postId,
+    Content: content,
+  });
 };
--- src/api/rest/reaction.ts
+++ src/api/rest/reaction.ts
@@ -1,16 +1,6 @@
 import client from "../client";
-import { API_TOKEN } from "./comment";
 
 export const getReactionTypes = async () => {
-  const response = await client.post(
-    '/Post/ReactionTypesGet',
-    {},
-    {
-      headers: {
-        "Content-Type": "application/json",
-        "X-Token": API_TOKEN,
-      },
-    }
-  );
+  const response = await client.post("/Post/ReactionTypesGet", {});
   return response.data;
 };
