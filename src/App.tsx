3c9e1a3eac152864b2f9ed86bd961894e4434f66
amiko <amikomiquchadze1@gmail.com>
2025-06-19 19:07:06 +0400

    register user provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import setupInterceptors from "./api/interceptors";
 import Dashboard from "./pages/dashboard/Dashboard";
-
+import { UserProvider } from "./contexts/UserContext";
 function App() {
-  setupInterceptors()
-  return <Dashboard />;
+  setupInterceptors();
+  return (
+    <UserProvider>
+      <Dashboard />
+    </UserProvider>
+  );
 }
 
 export default App;
