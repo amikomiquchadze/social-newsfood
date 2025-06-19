e0df90d4e7de1551b8b415d6f67300c4a25abada
amiko <amikomiquchadze1@gmail.com>
2025-06-19 22:47:31 +0400

    feat: Reactions

---
+++ src/components/postcard/components/reactions/Reactions.styled.tsx
@@ -0,0 +1,43 @@
+import styled from "@emotion/styled";
+import { Colors } from "../../../../styles/colors";
+
+const { black } = Colors;
+
+export const MetaRow = styled.div`
+  position: relative;
+  margin-top: 0.8rem;
+  font-size: 0.8rem;
+  color: ${black};
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Reactions = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 0.85rem;
+  gap: 1px;
+  cursor: pointer;
+`;
+
+export const ReactionPopupWrapper = styled.div`
+  position: absolute;
+  bottom: 100%;
+  left: 0;
+  margin-bottom: 10px;
+  background: ${black};
+  color: ${black};
+  border-radius: 12px;
+  padding: 0.75rem 1rem;
+  box-shadow: 0 6px 18px rgba(0, 0, 0, 0.4);
+  z-index: 1000;
+  min-width: 180px;
+`;
+
+export const ReactionEmoji = styled.img`
+  width: 16px;
+  height: 16px;
+  margin-right: -3px;
+  vertical-align: middle;
+`;
---
+++ src/components/postcard/components/reactions/Reactions.tsx
@@ -0,0 +1,103 @@
+import { useEffect, useRef, useState } from "react";
+import * as S from "./Reactions.styled";
+import ReactorViewer, { Reactor } from "../reactors/ReactorsViewer";
+import api from "../../../../api";
+
+interface ReactionsProps {
+  reactionCounts: any;
+  reactionOptions: { type: string; emoji: string }[];
+  post: { PostID: number };
+}
+export default function Reactions({
+  reactionCounts,
+  reactionOptions,
+  post,
+}: ReactionsProps) {
+  const [showReactionViewer, setShowReactionViewer] = useState(false);
+  const popupRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        popupRef.current &&
+        !popupRef.current.contains(event.target as Node)
+      ) {
+        setShowReactionViewer(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const [reactors, setReactors] = useState<Reactor[]>([]);
+
+  const fetchReactors = async () => {
+    try {
+      const resp = await api.reactions.getReactors(post?.PostID);
+      if (resp) {
+        setReactors(resp);
+      }
+    } catch (err) {
+      console.error("Failed to fetch reactors", err);
+    }
+  };
+
+  useEffect(() => {
+    fetchReactors();
+  }, [post.PostID]);
+
+  return (
+    <>
+      <S.MetaRow>
+        <div style={{ position: "relative" }}>
+          <S.Reactions
+            onClick={() => setShowReactionViewer((prev: any) => !prev)}
+          >
+            {Object.entries(reactionCounts)
+              .filter(([, val]: any) => val > 0)
+              .slice(0, 3)
+              .map(([key]) => {
+                return (
+                  <S.ReactionEmoji
+                    key={key}
+                    src={
+                      reactionOptions.find((r: any) => r.type === key)?.emoji
+                    }
+                    alt={key}
+                  />
+                );
+              })}
+            {(() => {
+              const total: number = Object.values(
+                reactionCounts as Record<string, number>
+              ).reduce((a, b) => a + b, 0);
+              if (total === 1) {
+                return (
+                  <span style={{ marginLeft: 4, fontSize: "0.85rem" }}>
+                    {reactors[0]?.FirstName + " " + reactors[0]?.LastName}
+                  </span>
+                );
+              } else if (total > 1) {
+                return (
+                  <span style={{ marginLeft: 4, fontSize: "0.85rem" }}>
+                    {reactors[0]?.FirstName + " " + reactors[0]?.LastName} and{" "}
+                    {total - 1} others
+                  </span>
+                );
+              }
+              return null;
+            })()}
+          </S.Reactions>
+
+          {showReactionViewer && (
+            <S.ReactionPopupWrapper ref={popupRef}>
+              <ReactorViewer reactors={reactors} />
+            </S.ReactionPopupWrapper>
+          )}
+        </div>
+      </S.MetaRow>
+    </>
+  );
+}
