6a78bee691d843a60fc4e9b23a7c027983bf6ae9
amiko <amikomiquchadze1@gmail.com>
2025-06-19 19:45:20 +0400

    get and post methods for comments

--- src/api/rest/comment.ts
+++ src/api/rest/comment.ts
@@ -1,5 +1,20 @@
 import client from "../client";
 
+export interface CreateCommentPayload {
+  PostID: number;
+  Content: string;
+}
+
+export const createComment = async (data: CreateCommentPayload) => {
+  const response = await client.post("/Comment/Create", data);
+  return response.data;
+};
+
+export const getCommentsByPostId = async (postId: number) => {
+  const response = await client.post("/Comment/GetAll", { PostID: postId });
+  return response.data;
+};
+
 export const deleteComment = async (commentId: number) => {
   const response = await client.post("/Comment/Delete", {
     CommentID: commentId,
