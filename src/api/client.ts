4f1957a25840f4996a7247b86724b0e51a57ad5a
amiko <amikomiquchadze1@gmail.com>
2025-06-19 15:53:06 +0400

    extend axios

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,6 +1,4 @@
-import axios from "axios"
-
-const apiKey = process.env.REACT_APP_SOCIALNEWsFEED_KEY
-
-const client = axios.create({baseURL: apiKey})
-export default client
+import axios from "axios";
+const apiKey = process.env.REACT_APP_SOCIALNEWSFEED_KEY;
+const client = axios.create({ baseURL: apiKey });
+export default client;
